fdd1d7ff3f3697a9595ce4744059a20f94b1e304
lorenzo.crecco <lorenzo.crecco@crea.gov.it>
2025-09-04 19:09:01 +0200

    testing pre-commit

--- pypackage_example/pypackage_example.py
+++ pypackage_example/pypackage_example.py
@@ -8,6 +8,7 @@ class LeafletMap(ipyleaflet.Map):
         super().__init__(center=center, zoom=zoom, **kwargs)
         self.layout.height = height
 
+
     def add_basemap(self, basemap="OpenStreetMap"):
         basemaps = {
             "OpenStreetMap": ipyleaflet.basemaps.OpenStreetMap.Mapnik,
@@ -36,3 +37,8 @@ class LeafletMap(ipyleaflet.Map):
         url = eval(f"ipyleaflet.basemaps.{basemap}").build_url()
         layer = ipyleaflet.TileLayer(url=url, name=basemap)
         self.add(layer)
+
+    
+    def dummy_method(self):
+        pass
+
