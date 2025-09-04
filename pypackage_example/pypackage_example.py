9f5c7cc0fef716356ebe28ae95c06e5a83ed0a16
lorenzo.crecco <lorenzo.crecco@crea.gov.it>
2025-09-04 16:14:10 +0200

    update

--- pypackage_example/pypackage_example.py
+++ pypackage_example/pypackage_example.py
@@ -7,27 +7,30 @@ class LeafletMap(ipyleaflet.Map):
     def __init__(self, center=[20, 0], zoom=2, height="400px", **kwargs):
         super().__init__(center=center, zoom=zoom, **kwargs)
         self.layout.height = height
-    
-    
+
     def add_basemap(self, basemap="OpenStreetMap"):
         basemaps = {
             "OpenStreetMap": ipyleaflet.basemaps.OpenStreetMap.Mapnik,
-            #"Stamen Terrain": ipyleaflet.basemaps.Stamen.Terrain,
-            #"Stamen Toner": ipyleaflet.basemaps.Stamen.Toner,
+            # "Stamen Terrain": ipyleaflet.basemaps.Stamen.Terrain,
+            # "Stamen Toner": ipyleaflet.basemaps.Stamen.Toner,
             "CartoDB Positron": ipyleaflet.basemaps.CartoDB.Positron,
             "CartoDB DarkMatter": ipyleaflet.basemaps.CartoDB.DarkMatter,
             "OpenTopoMap": ipyleaflet.basemaps.OpenTopoMap,
-            "Esri WorldImagery": ipyleaflet.basemaps.Esri.WorldImagery
-            }
+            "Esri WorldImagery": ipyleaflet.basemaps.Esri.WorldImagery,
+        }
         if basemap in basemaps:
             # tile_layer = basemaps[basemap]
             # url = tile_layer['url']
-            # attribuition = tile_layer['attribution']
-            tile_layer = ipyleaflet.TileLayer(url=basemaps[basemap]['url'], attribution=basemaps[basemap]['attribution'])
+            # attribution = tile_layer['attribution']
+            tile_layer = ipyleaflet.TileLayer(
+                url=basemaps[basemap]["url"],
+                attribution=basemaps[basemap]["attribution"],
+            )
             self.add_layer(tile_layer)
         else:
-            raise ValueError(f"Basemap '{basemap}' not recognized. Available options: {list(basemaps.keys())}")
-        
+            raise ValueError(
+                f"Basemap '{basemap}' not recognized. Available options: {list(basemaps.keys())}"
+            )
 
     def add_basemap2(self, basemap="OpenTopoMap"):
         url = eval(f"ipyleaflet.basemaps.{basemap}").build_url()
